11ea9443accc6b81cc1080517a7b4fad4abcfc98
Angel Alexander <angale22@gmail.com>
2020-08-26 17:57:08 -0500

    redid entire repo for gitignore file

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
-
-const writeFileAsync = util.promisify(fs.writeFile);
-
 // array of questions for user
 const questions = [
 
