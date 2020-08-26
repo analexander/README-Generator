88c6056498b5ab26ce2f714bad88cab0decfd82b
Angel Alexander <angale22@gmail.com>
2020-08-26 18:11:00 -0500

    adding promisify

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
 // array of questions for user
 const questions = [
 
