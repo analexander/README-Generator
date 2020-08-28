fa2b374da1ee5c459f074212a79c85f6619f727e
Angel Alexander <angale22@gmail.com>
2020-08-28 18:53:49 -0500

    added path package to index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
+const path = require("path");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 const writeFileAsync = util.promisify(fs.writeFile);
